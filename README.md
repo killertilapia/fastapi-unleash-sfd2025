4e410f0f0bcb9bae3a0bfb8ebe26f3d5480f55e8
Jaypax Ginete <killer.tilapia@gmail.com>
2025-09-13 09:17:44 +0800

    Add UnleashClient as dep Updated api.py to use UnleashClient Updated README.md

---
+++ README.md
@@ -0,0 +1,37 @@
+# Fastapi with Unleash SDK Demo App
+
+This is a simple FastAPI app to demo feature flags with Unleash SDK For SFD 2025 Bukidnon.
+
+# Requirements
+
+1. Docker
+2. Python (^3.13)
+3. Poetry
+
+# Instructions
+
+1. Clone [Unleash-Docker](https://docs.getunleash.io/using-unleash/deploy/getting-started) and get that running. This is your Unleash server.
+
+2. Clone this repo and run 
+
+    `# poetry install`. 
+
+    Don't forget to activate the virtual environment after:
+
+    `# eval $(poetry env activate)`
+
+3. Login to your Unleash server and setup your first feature flag. There should be only one project - `default` - on the dashboard.
+
+4. Make sure the feature flag NAME matches with one in the fastapi app. Change it to something different if you want. 
+
+    Don't forget to set the API key on the fastapi app.
+
+5. Use an REST API client like Insomnia, Bruno or Postman to test functionality
+
+# Author
+
+Jaypax Ginete <killer.tilapia@gmail.com>
+
+[My Blog](https://killertilapia.blogspot.com)
+
+[Presentation](https://google.com)
--- api.py
+++ api.py
@@ -1,18 +1,34 @@
+import logging
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from UnleashClient import UnleashClient
 
-app = FastAPI()
+#logging.basicConfig(level=logging.INFO)
+#logger = logging.getLogger(__name__)
+
+unleash_client = UnleashClient(
+    url="http://localhost:4242/api/",
+    app_name="unleash-onboarding-python",
+    custom_headers={'Authorization': 'default:development.unleash-insecure-api-token'}) 
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # startup here - load models, singletons, etc.
-
+    # startup here - load models, singletons, etc.    
+    unleash_client.initialize_client()
+    
+    # Test Unleash Client is connected and working
+    print(f"Feature flags working - {unleash_client.is_enabled('experiment-1')}")
     # //end of startup
-    yield
-    # shutdown here - release locks, del temps, etc.
 
+    yield
+    
+    # shutdown here - release locks, del temps, etc.
+    unleash_client.destroy()
     # //end of shutdown
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/ping")
@@ -27,4 +43,7 @@ async def feature_available():
 
 @app.get("/experiment")
 async def experiment_feature():
-    return {"msg": "This feature is experimental and not generally available"}
+    if(unleash_client.is_enabled('experiment-1')):
+        return {"msg": "This feature is experimental and not generally available"}
+    else:
+        raise HTTPException(status_code=404, detail="Resource not found!")
